e001f8710934dc619115f6a14ea4e0d5eae7b935
Fili258-oss <marinorubianobotina@gmail.com>
2026-08-10 22:00:47 -0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { iniriaSerif } from "@/app/ui/fonts";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,11 +9,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="es" className={`${iniriaSerif.variable}`} >
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <body className="font-iniria-serif font-normal antialiased">{children}</body>
     </html>
   );
 }
---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,18 @@
+import localFont from "next/font/local";
+export const iniriaSerif = localFont({
+    src: [
+        // Thin (100)
+        { path: './fonts/iniria-serif/InriaSerif-Regular.woff2', weight: '100', style: 'normal' },
+        { path: './fonts/iniria-serif/InriaSerif-Italic.woff2', weight: '100', style: 'italic' },
+        // Light (300)
+        { path: './fonts/iniria-serif/InriaSerif-Light.woff2', weight: '300', style: 'normal' },
+        { path: './fonts/iniria-serif/InriaSerif-LightItalic.woff2', weight: '300', style: 'italic' },
+        // Regular (400)
+        { path: './fonts/iniria-serif/InriaSerif-Regular.woff2', weight: '400', style: 'normal' },
+        // Bold (700)
+        { path: './fonts/iniria-serif/InriaSerif-Bold.woff2', weight: '700', style: 'normal' },
+        { path: './fonts/iniria-serif/InriaSerif-BoldItalic.woff2', weight: '700', style: 'italic' },        
+    ],
+    variable: '--font-iniria-serif',
+    display: 'swap',
+});
