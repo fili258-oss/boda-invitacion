b022a6bc463c58065c55da22ba706854ed8ec804
Fili258-oss <marinorubianobotina@gmail.com>
2026-08-20 00:31:49 -0500

    refactor(SectionFour): update layout and improve image positioning

--- src/app/ui/sections/SectionFour.tsx
+++ src/app/ui/sections/SectionFour.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function SectionFour() {
   return (
-    <div className="flex flex-wrap items-center relative h-dvh w-full overflow-hidden">
+    <div className="flex flex-col relative h-dvh w-full overflow-hidden">
       <Image
         src="/assets/images/backgrounds/PNGFondo/SectionOne.avif"
         alt="Fondo de la sección cuatro"
@@ -11,29 +11,30 @@ export default function SectionFour() {
         priority
         className="z-10 h-full w-full object-cover"
       />
-      <div className="flex flex-wrap basis-5/5 z-20 justify-center gap-4 px-8">
+      <div className="flex flex-wrap basis-auto z-20 justify-center items-center px-8">
         <Image
           src="/assets/images/elements/17.avif"
           alt="Adorno de la sección cuatro"
-          className="z-20 object-contain"
+          className="top-4 relative z-20 object-contain"
           width={350}
           height={100}
         />
         <Image
           src="/assets/images/elements/AngeloCaroG.avif"
           alt="Foto de Angelo y Carolina"
-          className="-top-25 relative z-20 object-contain"
+          className="-top-16 relative z-20 object-contain"
           width={250}
           height={100}
         />
         <Image
           src="/assets/images/elements/18.avif"
           alt="Adorno de la sección cuatro"
-          className="relative -top-25 z-20 object-contain"
+          className="-top-12 relative z-20 object-contain"
           width={250}
           height={100}
-        />
+        />        
       </div>
+      <div className='clear-both mt-10'></div>
     </div>
   );
 }
